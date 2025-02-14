3d7180f1b62d70832aa1dcc853eefddaebcc178a
Hemantkumar078 <hemantjatav023@gmail.com>
2025-02-14 15:17:36 +0000

    conversion comparison

---
+++ 01_basics_js/comparisions.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);// here >= ,> ,<,<= comparison operators converts string to number and  then compares 
+
+// console.log(null > 0);
+// console.log(null == 0);// but == operators works diffrently it not converts null to number
+// console.log(null >= 0);// here > ,>= comparison operator converts null to 0(means number )
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);// so undefined is undefined not convertable(or not comparable) to anything so it gives false
+
+// // === 
+
+ console.log("2" === 2);// so here triple equal(===) sign also compares datatype with value of both sides if datatype not same it gave false

0537681203c90ed796871c95f66e1c4abfdd0a45
Hemantkumar078 <hemantjatav023@gmail.com>
2025-02-23 23:32:13 +0000

    basics

---
+++ 01_basics_js/string.js
@@ -0,0 +1,38 @@
+const name = "hitesht"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('hitesh-hc-com')
+console.log(name.slice(-4,4));
+console.log(name);
+// // console.log(gameName[0]);
+// // console.log(gameName.__proto__);
+
+
+// // console.log(gameName.length);
+// // console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+ //console.log(newStringOne.trim());
+ //console.log(newStringOne.trim());
+//console.log(newStringOne);
+ const url = "https://hitesh.com/hitesh%20choudhary"
+
+ console.log(url.replace('%20', '-'))//it replaces the first '%20' (or whatever given ) with '-' (or whatever given that side) 
+ console.log(url);
+
+// console.log(url.includes('sundar'))//check if that string present inside it or not
+
+// console.log(gameName.split('-'));//return an array based on split of ('') what we give inside it

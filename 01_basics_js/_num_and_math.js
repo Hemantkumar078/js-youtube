40ba60a3c5103ca8a33b02d77a9433ca3cd495f7
Hemantkumar078 <hemantjatav023@gmail.com>
2025-03-12 18:25:49 +0530

    Update _num_and_math.js

--- 01_basics_js/_num_and_math.js
+++ 01_basics_js/_num_and_math.js
@@ -4,12 +4,12 @@ const score = 400
 const balance = new Number(100)
 // console.log(balance);
 
-// console.log(balance.toString().length);
+// console.log(balance.toString().length);// toString() for to convert number to string 
 // console.log(balance.toFixed(1));// to fixed the number of digits after decimal
 
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));// gives precsion means only 4 digits from left (may include after decimal also)
+// console.log(otherNumber.toPrecision(4));// gives precision means only 4 digits from left (may include after decimal also)
 
 const hundreds = 1000000
 // console.log(hundreds.toLocaleString('en-IN'));// to convert that number in indian standard (if 'en-IN' not provided than it convert it to default US standard)
@@ -19,9 +19,9 @@ const hundreds = 1000000
 
 // console.log(Math);
 // console.log(Math.abs(-4));
-// console.log(Math.round(4.6));
-// console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
+// console.log(Math.round(4.6));//5
+// console.log(Math.ceil(4.2));//5
+// console.log(Math.floor(4.9));//4
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
@@ -32,4 +32,4 @@ console.log(Math.floor(Math.random()*10) + 1);
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)// to get random number between min and max

1bcb7f503f0a16147428600842ecbc9f8d126b7d
Hemantkumar078 <hemantjatav023@gmail.com>
2025-05-26 16:32:57 +0000

    changes

--- 01_basics_js/04_comparisions.js
+++ 01_basics_js/04_comparisions.js
@@ -5,8 +5,8 @@
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);// here >= ,> ,<,<= comparison operators converts string to number and  then compares 
+ console.log("2" > 1);//true  if one operand is a string and the other is a number, JavaScript tries to convert the string to a number before comparing.
+console.log("02" > 1);// true here >= ,> ,<,<= comparison operators converts string to number and  then compares 
 
 // console.log(null > 0);
 // console.log(null == 0);// but == operators works diffrently it not converts null to number
--- 01_basics_js/05_num_and_math.js
+++ 01_basics_js/05_num_and_math.js
@@ -6,14 +6,15 @@ const balance = new Number(100)
 
 // console.log(balance.toString().length);// toString() for to convert number to string 
 // console.log(balance.toFixed(1));// to fixed the number of digits after decimal
-
+//Variables and functions usually start with a lowercase letter (camelCase).
+//Classes and constructor functions start with a capital letter (PascalCase).
 const otherNumber = 123.8966
 
 // console.log(otherNumber.toPrecision(4));// gives precision means only 4 digits from left (may include after decimal also)
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));// to convert that number in indian standard (if 'en-IN' not provided than it convert it to default US standard)
-
+ console.log( hundreds.toLocaleString('en-IN'));// to convert that number in indian standard (if 'en-IN' not provided than it convert it to default US standard)
+//type after toLocaleString is string
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
@@ -23,6 +24,8 @@ const hundreds = 1000000
 // console.log(Math.ceil(4.2));//5
 // console.log(Math.floor(4.9));//4
 // console.log(Math.min(4, 3, 6, 8));
+const arr=[1,2,3,6,8.2,4,5];
+console.log(Math.max(...arr));
 // console.log(Math.max(4, 3, 6, 8));
 
 console.log(Math.random());
